73c41467bc8dfe9c224f4464d5ba38dfa08eb2b0
saltyseadoggo <ncamidge@protonmail.com>
2022-02-11 21:50:26 -0800

    :rdj: I have no fucking clue how surface rules work

--- src/main/java/io/github/saltyseadoggo/blazingdepths/terrablender/BlazingDepthsTerraBlenderImpl.java
+++ src/main/java/io/github/saltyseadoggo/blazingdepths/terrablender/BlazingDepthsTerraBlenderImpl.java
@@ -1,10 +1,12 @@
 package io.github.saltyseadoggo.blazingdepths.terrablender;
 
+import net.minecraft.block.Blocks;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.source.util.MultiNoiseUtil;
+import net.minecraft.world.gen.surfacebuilder.MaterialRules;
 
 import com.mojang.datafixers.util.Pair;
 
@@ -13,7 +15,9 @@ import terrablender.api.BiomeProvider;
 import terrablender.api.BiomeProviders;
 import terrablender.api.TerraBlenderApi;
 import terrablender.worldgen.TBClimate;
+import terrablender.worldgen.TBSurfaceRuleData;
 
+import java.util.Optional;
 import java.util.function.Consumer;
 
 public class BlazingDepthsTerraBlenderImpl implements TerraBlenderApi
@@ -23,17 +27,35 @@ public class BlazingDepthsTerraBlenderImpl implements TerraBlenderApi
     Identifier blazing_depths_biome_provider;
 
     @Override
+	public Optional<MaterialRules.MaterialRule> getDefaultNetherSurfaceRules() {
+		return Optional.of(
+			MaterialRules.sequence(
+				MaterialRules.condition(
+					MaterialRules.biome(BlazingDepthsBiomes.SEARED_DUNES_KEY),
+					MaterialRules.sequence(
+                        MaterialRules.condition(MaterialRules.STONE_DEPTH_FLOOR, MaterialRules.block(Blocks.RED_SAND.getDefaultState())),
+                        MaterialRules.condition(MaterialRules.STONE_DEPTH_FLOOR_WITH_SURFACE_DEPTH, MaterialRules.block(Blocks.RED_SANDSTONE.getDefaultState()))
+                    )
+                ),
+				TBSurfaceRuleData.nether()
+			)
+		);
+	}
+
+    @Override
     public void onTerraBlenderInitialized() 
     {
-        BiomeProviders.register(new BlazingDepthsBiomeProvider(blazing_depths_biome_provider, 1));
+            //The former is the overworld provider weight; the latter is the nether provider weight.
+            //Because Blazing Depths doesn't add any overworld biomes, the former value must always be zero, or the game crashes!
+        BiomeProviders.register(new BlazingDepthsBiomeProvider(blazing_depths_biome_provider, 0, 1));
     }
 
         //Class in a class? I didn't know this was possible but this is how the Terra Blender docs say to do it.
     public class BlazingDepthsBiomeProvider extends BiomeProvider {
             //The Terra Blender documentation uses the Mojang mappings class name ResourceLocation. With Yarn, it is Identifier.
-        public BlazingDepthsBiomeProvider(Identifier name, int weight)
+        public BlazingDepthsBiomeProvider(Identifier name, int overworldWeight, int netherWeight)
         {
-            super(name, weight);
+            super(name, overworldWeight, netherWeight);
         }
 
         @Override
