9af5d7089ddbc85e209ef9408b7dd7015e25c001
saltyseadoggo <ncamidge@protonmail.com>
2022-02-17 21:37:24 -0800

    fixed seared sandstone item model, hopefully fix loopsound crash

--- src/main/java/io/github/saltyseadoggo/blazingdepths/init/BlazingDepthsSoundEvents.java
+++ src/main/java/io/github/saltyseadoggo/blazingdepths/init/BlazingDepthsSoundEvents.java
@@ -6,12 +6,10 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class BlazingDepthsSoundEvents {
-    public static SoundEvent AMBIENT_SEARED_DUNES_LOOP;
+    public static SoundEvent AMBIENT_SEARED_DUNES_LOOP = register(BlazingDepths.makeIdentifier("ambient.seared_dunes.loop"));
 
-	//Initialization event called by BlazingDepths.java
-    public static void init() {
-        AMBIENT_SEARED_DUNES_LOOP = register(BlazingDepths.makeIdentifier("ambient.seared_dunes.loop"));
-    }
+	//Init event called by BlazingDepths.java. Just like in BlazingDepthsBiomes, calling an empty init event seems to run the code in the class.
+    public static void init() {}
 
 	private static SoundEvent register(Identifier identifier) {
 		return Registry.register(Registry.SOUND_EVENT, identifier, new SoundEvent(identifier));
